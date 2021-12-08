fe65eb3c310ad9e9afb4c9dbda3b5b80378355fc
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2021-12-08 09:36:30 +0700

    Create Creative.js

---
+++ Js/Creative.js
@@ -0,0 +1,79 @@
+<script type="text/javascript">
+/*!
+ * Creative Design by @kang Wahyu
+ * Free for change but keep the original attribution.
+ * URL: https://alhikmah.my.id ; www.kata-h.blogspot.com ; wahyu9kdl.github.io ; awsos.blogspot.com ; https://youtube.com/channel/UC7CRa3nkxakAZx_aRsMwRyA ; https://m.facebook.com/Awgroupchannel/ ;https://about.me/ahmadwahyudi ; https://onedrive.live.com/?authkey=%21AIiRepe9LlR6x-Q&cid=B3548BE32AE66535&id=B3548BE32AE66535%21610&parId=B3548BE32AE66535%21288&o=OneUp ; https://www.dropbox.com/request/8i3MLGANkBstGwNii42Q ; https://youtu.be/P-Ko2exjawo ; https://youtu.be/4dIRWzDLjFM ; https://wahyu9kdl.github.io/AL-HIKMAH/ ; https://wahyu9kdl.github.io/DASHBOARD/ ; https://codepen.io/SAN3 ; 
+ * Donasi : https://link.dana.id/qr/3854e4rk ; https://paypal.me/wahyudi9kdl?locale.x=id_ID ; https://saweria.co/AwGroupChannel ; https://www.patreon.com/join/Awfanspage/signup?ru=%2Fjoin%2FAwfanspage%2Fcheckout%3Frid%3D0%26 ; 
+ * Youtube : https://youtu.be/gAxIFPagd4s , https://youtu.be/_ZQrNzHK6gs , https://youtu.be/LlfqzADGcF0 , https://youtu.be/k1hJrh09RGg , https://youtu.be/vzIAToNCWR0, https://youtu.be/twq6zIBYnis , https://youtu.be/thLGeHCZPcQ , https://youtu.be/mDzoG66bnIU , https://youtu.be/1D8x2SvHb8w , https://youtu.be/JMzBMqlaDhI , https://youtu.be/P-Ko2exjawo , https://youtu.be/4dIRWzDLjFM , https://youtu.be/KHOekzsKAfE , https://youtu.be/MtBrRUwLoJY , https://youtu.be/5Nx7ObRnCvc , https://youtu.be/QQM7gm27rd4 , https://youtu.be/okm6bkVPixw , https://youtu.be/H2ZKn4aMaAQ , https://youtu.be/H2ZKn4aMaAQ , https://youtu.be/In7lmwxrB5k ,
+ * Partner : 
+ 💠 Hosting dan domain 💠
+ 1. https://asia.cloudns.net/aff/id/482326
+ 2. https://panel.niagahoster.co.id/ref/361770
+ 3. https://billing.exabytes.co.id/aff.php?aff=8303039
+ 4. https://www.site123.com/?aff=7141773
+ 💠 Jasa Backlink 💠
+ https://rajabacklink.com/refferal.php?q=06a50e3f66db4a334202d3adfd31c589a334837978b494479f
+ 
+ 💠 Jasa Seo 💠
+ https://www.seobility.net/?px=2&a_aid=5fc75c87d70c2
+ 
+ Trafik exchange
+ 🟢 http://hit2hit.com/?rid=173879
+ 🟢 https://www.traffboost.net?ref=203557
+ 🟢 https://www.easyhits4u.com/?ref=wahyudi9
+ ⚙Publisher website 
+ 🟡1.Adsterra 
+ https://publishers.adsterra.com/referral/YM6H4bWeLn
+ 🟡2. propellerads
+ https://propellerads.com/publishers/?ref_id=msQP 
+ 🟡3. Accestrade 
+ https://imp.accesstrade.co.id/img.php?rk=0009ee000n9y
+ Daftar : https://kata-h.blogspot.com/2020/12/program-referral.html
+ 🟡4.Ads.ly
+ https://join-adf.ly/24900237
+ 🟢 5. Shareasale
+ https://shareasale.com/r.cfm?b=40&u=2751656&m=47&urllink=&afftrack=
+ 
+ 📡Job Bikin Project📡
+ 🔰Job freelancer🔰
+ https://www.freelancer.com/get/KATAHIKMAH?f=give
+ 
+ 🔺️ Aplikasi bikin sendiri 🔺️
+ ✔http://appsgeyser.com/widgetdownload.php?widget=KATA%20KATA%20HIKMAH%20AL%20HIKMAH_12877712
+ 
+ ➖📡Cara Bikin Toko Online ? Mau ?📡
+ Klik link berikut.
+ http://asistentoko.com/ref=Ahmad%20wahyudi
+ 
+ 🔰 Download File Gratis 🔰
+ Ref. https://sfile.mobi/inv.php?id=358103
+ https://kata-h.blogspot.com/p/daftar-campaign-affiliasi-website-kami.html?m=1
+ https://gudanggame.page.link/7xdQ
+ ✅Pilihan link yang bisa digunakan
+ https://sociabuzz.com/wahyu9kdl/donate
+ https://sociabuzz.com/wahyu9kdl/support
+ https://sociabuzz.com/wahyu9kdl/give
+ https://sociabuzz.com/proaccount/transaction
+ https://patreon.com/invite/wapwbg_2
+ https://www.patreon.com/Awfanspag
+ https://www.patreon.com/join/Awfanspage/checkout?ru=undefined
+ https://www.fiverr.com/wahyudi9kdl?up_rollout=true
+ http://www.fiverr.com/s2/797de2b747 
+ https://php.id/ref/29412 / http://hikmah.skom.id/
+ https://www.udemy.com/share/105onM3@lOPRUb3VfF_f2VvD2zEeDwy00nLcb78O2vVthWnbwB6p81Pe-uvX_bbQ2zdV3A0GqA==/
+ https://www.udemy.com/share/100H373@4-b7PQwNJWUoexq2dpvDhpl-qRRXEkMkUQMc_jpg9HMPvvXjAZvobHs142J3SR17RQ==/
+ https://business.facebook.com/Awgroupchannel/?fref=nf
+ https://paidviewpoint.com/?r=wahyu9
+ https://me.ckzink.com/ref/Wahyu9
+ 📡 ©️2021 📡
+ CSE AW
+ https://cse.google.com/cse?cx=db5f08cfff3041318
+ Aw Cse Browser
+ https://cse.google.com/cse?cx=f0cd8247e1562a2c3
+ https://projects.co.id/public/past_projects/view/22ae18/membuat-website-cekresi
+ 
+ https://cse.google.com/cse?cx=4a26c594511b8cd0c#gsc.tab=0&gsc.sort=&gsc.q=Tag%20HTML%20%5B%20elemen%20dalam%20HTML%20terdiri%20dari%20tag%20pembuka%20dan%20tag%20...
+ https://cse.google.com/cse?cx=792e1c207270e2810
+ 
+ */
+ </script>
